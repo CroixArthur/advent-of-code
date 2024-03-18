2af204d07fb693f470266bca3b03ad4e142194fe
CroixArthur <arthur.croix.pro@gmail.com>
2024-03-18 11:26:41 +0100

    ✨ Exo3

---
+++ src/exo3/part1.rs
@@ -0,0 +1,77 @@
+use crate::utility;
+
+fn retreive_number(row: &mut Vec<char>, j: usize) -> i32 {
+  let mut num = String::from("");
+  let mut tj = j;
+
+  while tj > 0 && row[tj].is_numeric() {
+    tj -= 1;
+  }
+  if !row[tj].is_numeric() {
+    tj += 1;
+  }
+  while tj < row.len() && row[tj].is_numeric() {
+    num.push(row[tj]);
+    row[tj] = '.';
+    tj += 1;
+  }
+  match num.parse::<i32>() {
+    Ok(res) => {
+      return res;
+    },
+    Err(_) => return 0
+  }
+}
+
+fn find_surrounding_numbers(data: &mut Vec<Vec<char>>, i: usize, j: usize, sum: &mut i32) {
+  let neighbors = vec![
+    (-1, -1), (0, -1), (1, -1),
+    (-1,  0),          (1,  0),
+    (-1,  1), (0,  1), (1,  1)
+  ];
+
+  for (di, dj) in neighbors {
+    let ni = i as i32 + di;
+    let nj = j as i32 + dj;
+    if ni >= 0 && nj >= 0 && nj < data.len() as i32 {
+      if let Some(row) = data.get_mut(ni as usize) {
+        if let Some(cell) = row.get(nj as usize) {
+          if cell.is_numeric() {
+            *sum += retreive_number(row, nj as usize);
+          }
+        }
+      }
+    }
+  }
+}
+
+fn process_2d_array(data: &mut Vec<Vec<char>>) {
+  let mut sum = 0;
+
+  for i in 0..=data.len() - 1 {
+    for j in 0..=data[i].len() - 1 {
+      if !data[i][j].is_numeric() && data[i][j] != '.' {
+        find_surrounding_numbers(data, i, j, &mut sum);
+      }
+    }
+  }
+  println!("Sum: {:?}", sum);
+}
+
+pub fn run() {
+  let filepath = "src/inputs/3input.txt";
+  let mut content: Vec<Vec<char>> = vec![];
+  let lines = utility::get_file_lines(filepath);
+  
+  match lines {
+    Ok(value) => {
+      for line in value {
+        if let Ok(res) = line {
+          content.push(res.chars().collect())
+        }
+      }
+    }
+    Err(e) => println!("{e}")
+  }
+  process_2d_array(&mut content);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 pub mod exo1;
 pub mod exo2;
+pub mod exo3;
 pub mod utility;
 
 fn main() {
   // crate::exo1::run();
   // crate::exo2::run_part_one();
-  crate::exo2::run_part_two();
+  // crate::exo2::run_part_two();
+  // crate::exo3::part1::run();
+  crate::exo3::part2::run();
 }
